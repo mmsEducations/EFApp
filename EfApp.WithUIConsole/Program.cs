3fc7faec0f541780f144e92cd31918e34d7d0632
mmsmesut <m.mesutsincar@hotmail.com>
2024-04-21 14:44:01 +0300

    Entity framework from Designer , with database first approach

---
+++ EfApp.WithUIConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace EfApp.WithUIConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EducationConnectionStr context = new EducationConnectionStr();
+            var groups = context.Groups.ToList();
+
+            Console.WriteLine("Gruplar");
+            foreach (var group in groups)
+            {
+                Console.WriteLine(group.Name);
+            }
+        }
+    }
+}
