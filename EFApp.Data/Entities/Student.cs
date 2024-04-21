ea4fc23dcee1eb75933a3d631abcc0451ffdd7e8
mmsmesut <m.mesutsincar@hotmail.com>
2024-04-21 12:44:55 +0300

    Reapply "formui added to project"
    This reverts commit 22fcefd6cfb50473163a35f4385ebce9d67e8794.

--- EFApp.Data/Entities/Student.cs
+++ EFApp.Data/Entities/Student.cs
@@ -1,9 +1,10 @@
 ﻿namespace EFApp.Data
 {
     //1:Databasedeki tablonun kolonlarının 1'e bir maplanme işlemleri burda yapılır 
+    //Database'de bulunan tablonun bir karşılığı 
     public class Student
     {
-        public int StudentId { get; set; }
+        public int StudentId { get; set; } //Dbde iligli tabloda bulunan kolonun adı,tipi,null olma durumu 
         public string Name { get; set; }
         public string Lastname { get; set; }
         public int GroupId { get; set; }
---
+++ EFApp.FormUI/Form1.cs
@@ -0,0 +1,29 @@
+﻿using EFApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace EFApp.FormUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonList_Click(object sender, EventArgs e)
+        {
+            EducationDbContext context = new EducationDbContext();
+            List<Student> students = context.Students.ToList();
+
+            dataGridViewStudentList.DataSource = students;
+
+
+        }
+    }
+}
+
+
+//Linq To Entities 
